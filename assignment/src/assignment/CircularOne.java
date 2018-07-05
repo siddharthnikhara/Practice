62009b4366f71d598fa3c7adf5813ea6690ba3bf
siddharth.n <siddharth.n@192.168.1.8>
2018-07-05 15:12:18 +0530

    test

---
+++ assignment/src/assignment/CircularOne.java
@@ -0,0 +1,10 @@
+package assignment;
+
+public class CircularOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ assignment/src/assignment/CircularlinkedList.java
@@ -0,0 +1,55 @@
+package assignment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CircularlinkedList {
+
+	private ListNode last;
+	private int length;
+
+	class ListNode {
+		private ListNode next;
+		private int data;
+
+		public ListNode(int data) {
+			this.data = data;
+		}
+	}
+
+	public CircularlinkedList() {
+		last = null;
+		length = 0;
+
+	}
+
+	public int length() {
+		return length;
+	}
+
+	public boolean isEmpty() {
+		return length == 0;
+	}
+
+	public void createCircularLinkedList() {
+		ListNode first = new ListNode(1);
+		ListNode second = new ListNode(5);
+		ListNode third = new ListNode(10);
+		ListNode fourth = new ListNode(15);
+
+		first.next = second;
+		second.next = third;
+		third.next = fourth;
+		fourth.next = first;
+
+		last = fourth;
+
+	}
+
+	public static void main(String[] args) {
+		CircularlinkedList cll = new CircularlinkedList();
+		cll.createCircularLinkedList();
+
+	}
+
+}
